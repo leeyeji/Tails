22e5aa04b12972963f91250d95fa5f5200df76b3
leeyeji <miracle458393@gmail.com>
2014-09-25 14:48:59 +0900

    add 1
    1

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var trailX = 0;
+var trailY = 0;
+
+function setup()
+{
+	createCanvas(800, 600);
+	noFill();
+	stroke(255, 0, 0);
+}
+
+function draw()
+{
+	background(0);
+	// mouse ellipse
+	ellipse(mouseX, mouseY, 100, 100);
+
+	// trail ellipse
+	trailX = (mouseX + trailX * 9) / 10;
+	trailY = (mouseY + trailY * 9) / 10;
+	ellipse(trailX, trailY, 80, 80);
+}
